0cc4c9d389007c6563cdc35230d6e4929c9c02f9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-19 00:59:59 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -6,8 +6,20 @@ const HomePage = () => {
     <div>
         <div className="md:flex md:mx-24 mx-8 md:my-[10%] my-[30%]">
             <div className="w-full">
+                <h1 className="text-4xl">
+                    <span className='font-semibold text-[#536DFE]'>Hi, </span>
+                    <span className='font-semibold'>I am </span>
+                    <span className='font-semibold text-[#536DFE]'>Jehan Weerasuriya</span>
+                </h1>
+
+                <p className="my-4 text-xl">
+                    In here is my Work According to the Fullstack Developer, VScode Extensions Developer and NPM Package Developer
+                </p>
+
+                <a href="">
+                    <button className='bg-[#536DFE] py-4 px-8 rounded-full text-white font-semibold my-4'>Get Started</button>
+                </a>
                 
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit et dolore magnam ea itaque, nesciunt debitis enim beatae velit quam error autem, amet nostrum similique accusantium saepe quae rerum tempora!
             </div>
             <div className="w-full  md:block hidden">
                 <center>
