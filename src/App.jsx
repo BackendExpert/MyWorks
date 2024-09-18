819e7989d541b528af93fbb825cd8c595c985852
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-19 00:29:01 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FetchData from "./components/FetchFromURL/FetchData";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FetchData />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/components/FetchFromURL/FetchData.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FetchData = () => {
+  return (
+    <div>FetchData</div>
+  )
+}
+
+export default FetchData
