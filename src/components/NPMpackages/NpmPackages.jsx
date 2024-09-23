1d0899f32774008fd2a00538e98d1bc969d120fd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-23 20:59:17 +0530

    Update NpmPackages.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/NPMpackages/NpmPackages.jsx
+++ src/components/NPMpackages/NpmPackages.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const NpmPackages = () => {
   return (
-    <div>NpmPackages</div>
+    <div className='md:mx-24 mx-8'>
+        <div className="md:flex  md:my-[10%] my-[30%]">
+            
+        </div>
+    </div>
   )
 }
 
