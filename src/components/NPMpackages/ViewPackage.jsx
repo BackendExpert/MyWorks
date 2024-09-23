a9843f50cfb38b91e4018768631f698a4b331619
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-09-23 22:28:43 +0530

    Update

--- src/components/NPMpackages/ViewPackage.jsx
+++ src/components/NPMpackages/ViewPackage.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import NPMImg from '../../assets/Npms.png'
 
 const ViewPackage = () => {
+
+    const {id} = useParams()
+
+    const npmPackages = [
+        {
+            id: 1,
+            pckName: "JKCSS-Framework",
+            npmLink: "https://www.npmjs.com/package/@jehankandy/jkcss"
+        },
+        {
+            id: 2,
+            pckName: "React-JKcss",
+            npmLink: "https://www.npmjs.com/package/@jehankandy/React-JKcss"
+        },
+    ]
+
   return (
     <div className='md:mx-24 mx-8'>
         <div className="md:flex  md:my-[10%] my-[30%]">
+            <div className="w-full">
+                <h1 className="text-4xl">
+                    <span className='font-semibold text-[#536DFE]'>{id}</span>
+                    
+                </h1>
+
+                <p className="my-4 text-xl">
+                    Node Package Managers (NPM) That I Developed
+                </p>
+
+                <a href="">
+                    <button className='bg-[#536DFE] py-4 px-8 rounded-full text-white font-semibold my-4'>Get Started</button>
+                </a>
+                
+            </div>
+            <div className="w-full  md:block hidden">
+                <center>
+                    <img src={NPMImg} alt="" className='h-80 w-auto'/>
+                </center>
+            </div>
         </div>
     </div>
   )
