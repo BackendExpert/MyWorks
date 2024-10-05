52f95c73f8577349b2e4cf44c7bd608d688331f5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-05 11:10:52 +0530

    Update

--- src/components/NPMpackages/ViewPackage.jsx
+++ src/components/NPMpackages/ViewPackage.jsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import NPMImg from '../../assets/Npms.png'
+import { BsDot } from 'react-icons/bs'
 
 const ViewPackage = () => {
 
@@ -13,30 +14,64 @@ const ViewPackage = () => {
         {
             id: 1,
             pckName: "JKCSS-Framework",
-            npmLink: "https://www.npmjs.com/package/@jehankandy/jkcss"
+            npmLink: "https://www.npmjs.com/package/@jehankandy/jkcss",
+            pckDesc: "This NPM package develoeped for the First CSS Frmework in Sri Lanka. and the Utility-First Approach from Verison  v6.0.0 - 05 May 2024 Latest"
         },
         {
             id: 2,
             pckName: "React-JKcss",
-            npmLink: "https://www.npmjs.com/package/@jehankandy/React-JKcss"
+            npmLink: "https://www.npmjs.com/package/@jehankandy/react-jkcss",
+            pckDesc: "This NPM package develoeped for the First CSS Frmework in Sri Lanka Support for the ReactJS. Currently v1.2.0 - 01 June 2024"
+        },
+        {
+            id: 3, 
+            pckName: 'JKReact ICONS',
+            npmLink: 'https://www.npmjs.com/package/@jehankandy/jkreact-icons',
+            pckDesc: 'First Icons support for React that developed in Sri Lanka Currently v1.1.0 - 29 April 2024'
+
+        },
+        {
+            id: 4, 
+            pckName: 'jkmysql-easy',
+            npmLink: 'https://www.npmjs.com/package/jkmysql-easy',
+            pckDesc: 'This is the one of Backend Support NPM Package with Node.js in this package the developer not need to type sql Queries Currently v1.4.0 - 25 May 2024'
+        },
+        {
+            id: 5, 
+            pckName: 'login-signup-react'
+        },
+        {
+            id: 6, 
+            pckName: 'js-wysiwyg-editor'
         },
     ]
 
     return (
-        <div className='md:mx-24 mx-8'>
+        <div className='md:mx-24 mx-8 min-h-auto'>
             <div className="md:flex  md:my-[10%] my-[30%]">
                 <div className="w-full">
                     <h1 className="text-4xl">
                         <span className='font-semibold text-[#536DFE]'>{NamePCK}</span>
                     </h1>
 
-                    <p className="my-4 text-xl">
-                        Node Package Managers (NPM) That I Developed
-                    </p>
-
-                    <a href="">
-                        <button className='bg-[#536DFE] py-4 px-8 rounded-full text-white font-semibold my-4'>Get Started</button>
-                    </a>
+                    {
+                        npmPackages.map((npmPck) => {
+                            if (NamePCK === npmPck.pckName) {
+                                return (
+                                    <div key={npmPck.id}>
+                                        <p className="my-4 text-xl">
+                                            {npmPck.pckDesc}
+                                        </p>
+                                        <a href={npmPck.npmLink} target='_blank'>
+                                            <button className='bg-[#536DFE] py-4 px-8 rounded-full text-white font-semibold my-4'>Read More</button>
+                                        </a>
+                                        
+                                    </div>
+                                )
+                            }
+                            return null
+                        })
+                    }
                 </div>
                 <div className="w-full md:block hidden">
                     <center>
@@ -47,18 +82,7 @@ const ViewPackage = () => {
 
             <div className="my-12">
                 <div>
-                    {
-                        npmPackages.map((npmPck) => {
-                            if (NamePCK === npmPck.pckName) {
-                                return (
-                                    <div key={npmPck.id}>
-                                        <h1 className="text-2xl font-semibold">{npmPck.pckName} dasdasd</h1>
-                                    </div>
-                                )
-                            }
-                            return null
-                        })
-                    }
+
                 </div>
             </div>
         </div>
